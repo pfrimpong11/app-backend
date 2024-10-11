236ea1c0d545d4f6c7f01e01e9ed36594213172e
pfrimpong11 <frimpongprince2000@gmail.com>
2024-10-11 22:12:38 +0000

    server update

--- app.js
+++ app.js
@@ -36,4 +36,4 @@ app.use('/api', require('./routes/userRoutes'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server started on port ${PORT}`));
